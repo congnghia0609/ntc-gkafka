08f3d80b2e11cb7fb0d75e36c84ab31475c051aa
nghiatc <congnghia0609@gmail.com>
2020-06-14 16:38:27 +0700

    clean code

--- deps.sh
+++ deps.sh
@@ -10,5 +10,6 @@ go get -u github.com/tools/godep
 go get -u github.com/congnghia0609/ntc-gconf
 go get -u gopkg.in/confluentinc/confluent-kafka-go.v1/kafka
 go get -u github.com/natefinch/lumberjack
+go get -u github.com/google/uuid
 
 echo "Install dependencies complete..."
--- kproducer/kproducer.go
+++ kproducer/kproducer.go
@@ -2,11 +2,9 @@ package kproducer
 
 import (
 	"fmt"
-	"github.com/google/uuid"
 	"golang.org/x/net/context"
 	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
 	"ntc-gkafka/kconfig"
-	"os"
 	"sync"
 	"time"
 )
@@ -148,47 +146,4 @@ func CreateTopic(p *kafka.Producer, topic string, partitions int, replications i
 		fmt.Printf("%v\n", result)
 	}
 	a.Close()
-}
-
-func Start() {
-	topic := "streams-plaintext-input"
-	p, err := kafka.NewProducer(&kafka.ConfigMap{
-		"bootstrap.servers": "127.0.0.1:9092,127.0.0.1:9093",
-		"client.id":         "KProducer_" + uuid.New().String(),
-		"acks":              "1",
-	})
-	if err != nil {
-		fmt.Printf("Failed to create producer: %v\n", err)
-		os.Exit(1)
-	}
-	// Go-routine to handle message delivery reports and
-	// possibly other event types (errors, stats, etc)
-	go func() {
-		for e := range p.Events() {
-			switch ev := e.(type) {
-			case *kafka.Message:
-				if ev.TopicPartition.Error != nil {
-					fmt.Printf("Failed to deliver message: %v\n", ev.TopicPartition)
-				} else {
-					fmt.Printf("Successfully produced recored to topic %s partion [%d] @ offset %v\n", *ev.TopicPartition.Topic, ev.TopicPartition.Partition, ev.TopicPartition.Offset)
-				}
-			}
-		}
-	}()
-
-	for i := 0; i < 10; i++ {
-		recordKey := "alice"
-		recordValue := fmt.Sprintf("This is message %d", i)
-		fmt.Printf("Preparing to produce record: %s\t%s\n", recordKey, recordValue)
-		p.Produce(&kafka.Message{
-			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-			Key:            []byte(recordKey),
-			Value:          []byte(recordValue),
-		}, nil)
-	}
-
-	// Wait for all messages to be delivered
-	p.Flush(15 * 1000)
-	fmt.Printf("10 messages were produced to topic: %s!\n", topic)
-	p.Close()
 }
